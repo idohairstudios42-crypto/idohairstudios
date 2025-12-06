ed2c87ee08880d127cabad5183fdc879c4d282cd
celetrialPrince166 <princeayiku5@gmai.com>
2025-12-06 19:20:53 +0000

    Remove experimental options causing build trace issues

--- next.config.js
+++ next.config.js
@@ -40,10 +40,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-  },
   compress: true,
   poweredByHeader: false,
   
